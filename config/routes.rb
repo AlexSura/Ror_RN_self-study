d9c00e447709cdb5d49c3737c01095264b0275c3
AlexSura <yellowjazz@mail.ru>
2023-09-22 21:39:25 +0500

    landeng_ror6

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  get '/robots.txt', to: proc { |env| [200, {}, ["User-agent: *\nDisallow: /"]] }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
