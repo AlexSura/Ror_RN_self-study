747716d4fd4145227579b1040b607987f38056e8
AlexSura <yellowjazz@mail.ru>
2023-09-23 18:59:32 +0500

    landeng_ror7

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 bundle install
-yarn install
+npm install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rails server -p 3000
